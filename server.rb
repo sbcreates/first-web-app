87585f4868bea10239388c2d171a257afbc6b11a
sbcreates <drawsarahb@gmail.com>
2017-08-24 16:10:46 -0400

    Finished exercise and checked that everything works

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 
+
 get '/home' do
   erb :index
 end
@@ -9,5 +10,25 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'felines', 'K9s', 'kittens', 'mittens']
+
   erb :about_me
+end
+
+get '/favorites' do
+  @fav_links = [
+    "https://www.reddit.com/", "https://www.buzzfeed.com", "https://www.twitter.com", "https://www.facebook.com", "https://www.reddit.com/r/aww"
+  ]
+
+  erb :favorites
+end
+
+# REDIRECTS
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
 end
